1a5b05064e5a0641f5fbfed1777c42325e1c08e4
Daniel English <denglish7@gmail.com>
2017-03-16 19:33:04 -0700

    mongoose file changed

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,9 @@ var path = require('path');
 var models_path = path.join(__dirname + './../models');
 var fs = require('fs');
 
-mongoose.connect('mongodb://localhost/testDB');
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+mongoose.connect(mongoUri);
 
 fs.readdirSync(models_path).forEach(function(file){
     if(file.indexOf('.js') >= 0){
