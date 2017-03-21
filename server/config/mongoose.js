75eff8bfab909ea073dd3ff1dc1c99d49b9eaf5b
Daniel English <denglish7@gmail.com>
2017-03-21 11:10:36 -0700

    added connection to mlab

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,18 @@ var path = require('path');
 var models_path = path.join(__dirname + './../models');
 var fs = require('fs');
 
-mongoose.connect('mongodb://localhost/testDB');
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/testDB';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 fs.readdirSync(models_path).forEach(function(file){
     if(file.indexOf('.js') >= 0){
